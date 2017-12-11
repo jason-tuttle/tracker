adbd40183a1e0409d51ea03e675b829643ac025a
Jason Tuttle <jason@hearspotbark.com>
2017-12-10 22:04:42 -0500

    basic routing

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,20 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+  var bootstrapPath = 'bower_components/bootstrap-sass/assets/';
+
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: [
+        bootstrapPath + 'stylesheets/'
+      ]
+    }
   });
+
+  // Create paths to bootstrap assets
+  // Add assets to app with import
+  app.import(bootstrapPath + 'javascripts/bootstrap/collapse.js');
+  app.import(bootstrapPath + 'javascripts/bootstrap/transition.js');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
